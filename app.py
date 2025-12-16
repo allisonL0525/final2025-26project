673a03452ce7091684413b5b491c23fa8ca730b1
allisonL0525 <allysnowtail567@gmail.com>
2025-12-16 10:03:45 -0500

    add

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 # pls don't die
 print("Hello!")
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Welcome to our Flash Web Application"
+
+if __name__ == "__main__":
+    app.run(debug=True)
